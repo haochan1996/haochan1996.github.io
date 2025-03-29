460bc19b931fa46a53adb0b63c5090096f77f7d0
haochan1996 <espholychan@outlook.com>
2025-03-29 18:43:17 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,8 +22,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  //organizationName: 'facebook', // Usually your GitHub org/user name.
-  //projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'haochan1996', // Usually your GitHub org/user name.
+  projectName: 'haochan1996', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
